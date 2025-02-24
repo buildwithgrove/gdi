5dbcd50fe972b455ec938b2e109b6973e2ae1d9c
Pascal van Leeuwen <pascal@grove.city>
2025-02-24 08:50:43 +0000

    fix: add init script to add config file on first run

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -27,13 +29,22 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gdi",
-	Short: "Grove Developer Interface - streamline your development workflows",
-	Long: `Grove Developer Interface (GDI) is a comprehensive CLI tool designed for internal
-development at Grove. It provides users with a unified approach to manage configuration
-settings, execute Git operations, and interact with integrated LLM providers for tasks
-such as automated pull request generation.`,
+	Use:     "gdi",
+	Short:   "Grove Developer Interface - streamline your development workflows",
+	Long:    "", //Assigned in generateLongDescription()
 	Version: "0.0.1",
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Toggle verbose mode or other options")
+	rootCmd.AddCommand(git.GitCmd)
+	rootCmd.AddCommand(config.ConfigCmd)
+	rootCmd.Long = generateLongDescription()
+
+	if !config.ConfigExists() {
+		config.RunFirstTimeSetup()
+		return
+	}
 }
 
 func Execute() {
@@ -43,8 +54,26 @@ func Execute() {
 	}
 }
 
-func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Toggle verbose mode or other options")
-	rootCmd.AddCommand(git.GitCmd)
-	rootCmd.AddCommand(config.ConfigCmd)
+// generateLongDescription generates the long description for the root command.
+func generateLongDescription() string {
+	var sb strings.Builder
+	sb.WriteString(`Grove Developer Interface (GDI) is a comprehensive CLI tool designed for internal
+	development at Grove. It provides users with a unified approach to manage configuration
+	settings, execute Git operations, and interact with integrated LLM providers for tasks
+	such as automated pull request generation.
+
+	Available Commands:
+	`)
+	appendCommands(&sb, rootCmd, "")
+	return sb.String()
+}
+
+// appendCommands appends the commands to the long description.
+func appendCommands(sb *strings.Builder, cmd *cobra.Command, prefix string) {
+	for _, c := range cmd.Commands() {
+		if !c.Hidden {
+			sb.WriteString(fmt.Sprintf("%s  %-10s %s\n", prefix, c.Name(), c.Short))
+			appendCommands(sb, c, prefix+"  ")
+		}
+	}
 }
